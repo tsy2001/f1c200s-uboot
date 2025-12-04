fc0611e1beffe1656b484e97a4c5075034e542ac
tengsiyuan <tsy88123@outlook.com>
2025-12-04 03:06:01 -0500

    modify patition table

--- board/sunxi/board.c
+++ board/sunxi/board.c
@@ -696,7 +696,7 @@ static void setup_environment(const void *fdt)
 			mac_addr[4] = (sid[3] >>  8) & 0xff;
 			mac_addr[5] = (sid[3] >>  0) & 0xff;
 
-			eth_env_set_enetaddr(ethaddr, mac_addr);
+			// eth_env_set_enetaddr(ethaddr, mac_addr);
 		}
 
 		if (!env_get("serial#")) {
